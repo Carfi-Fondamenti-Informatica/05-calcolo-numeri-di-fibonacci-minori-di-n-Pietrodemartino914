5a8cf7138ddc6d4d6a7262d2a6a2161dbdcd8bf2
Pietrodemartino914 <115526682+Pietrodemartino914@users.noreply.github.com>
2022-11-26 10:47:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
 using namespace std;
-
 void fibonacci (int n) {
-    if (n == 2) {
+    if (n >= 2) {
         cout << "1" << endl;
         cout << "1" << endl;
-
-    } else if (n == 1) {
-        cout << "1" << endl;
-
-    } else if(n>2){
-        int risultato = 1, a , b = 1;
+        int risultato = 1, a, b = 1;
         while (n >= risultato) {
-            a=risultato;
-            risultato= a+b;
-            b=a;
+            a = risultato;
+            risultato = a + b;
+            b = a;
+        }
+        a=risultato-b;
+        risultato -= a;
+        b=1;
+        int x = 1;
+        while (x<risultato){
+            a = x;
+            x = a + b;
+            b = a;
+        cout << x << endl;
         }
 
-        while (risultato>1){
-            a = risultato-b;
-            risultato -=a;
-            b=a;
-            cout << risultato << endl;
-
-        }
+    }else if (n == 1) {
         cout << "1" << endl;
+        }
     }
-}
+
+
 int main() {
     int n =0;
     cin >> n;
